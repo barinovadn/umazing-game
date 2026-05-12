0e0873502cf012d13daa59b8bbde3b1b82cde60b
daria111222 <186514423+barinovadn@users.noreply.github.com>
2026-05-12 21:50:09 +0400

    feat: add glow effect to item pickups

--- prefabs/items/base_item.gd
+++ prefabs/items/base_item.gd
@@ -1,14 +1,31 @@
 extends Resource
 class_name ItemData
 
+enum Rarity { COMMON, RARE, EPIC }
+
+@export_group("Common")
+@export var rarity: Rarity = Rarity.COMMON
 @export var name: String = "Item"
 @export var icon: Texture2D
-@export var max_stack: int = 99 
+@export_multiline var description: String = ""
+
+@export_group("Amount")
+@export var max_stack: int = 99
 @export var amount: int = 1
-@export var description: String = ""
+
+@export_group("Flags")
 @export var is_consumable: bool = false
 @export var is_active: bool = false
 @export var is_stackable: bool = false
 
 
-func use(): pass
+func get_rarity_color() -> Color:
+	match rarity:
+		Rarity.COMMON: return Color.WHITE
+		Rarity.RARE: return Color.AQUA
+		Rarity.EPIC: return Color.DEEP_PINK
+	return Color.WHITE
+
+
+func use():
+	pass
--- prefabs/pickups/item_pickup.gd
+++ prefabs/pickups/item_pickup.gd
@@ -2,16 +2,25 @@ extends RigidBody2D
 class_name Pickup
 
 @export var item_data: ItemData
+@export var test_item: ItemData
 
-@onready var sprite = $Sprite2D
+@onready var sprite = $Icon
 @onready var inventory = %Player/%Inventory
+@onready var light = $Icon/RarityLight
 
 
 func _ready():
 	if item_data:
 		sprite.texture = item_data.icon
+	if test_item:
+		set_light_by_rarity(test_item)
 
-	
+
+func set_light_by_rarity(data: ItemData):
+	if light: 
+		light.color = data.get_rarity_color()
+
+
 func collect():
 	var leftover = inventory.add_item(item_data)
 	
