93d3cd9c3df7edc654c556552f9cf7e7fba4c653
kesha <110396687+I-am-cat04@users.noreply.github.com>
2026-05-14 23:38:33 +0400

    feat: Add typing animation

--- prefabs/dialogues/dialogue.gd
+++ prefabs/dialogues/dialogue.gd
@@ -2,9 +2,22 @@ class_name Dialogue
 extends Resource
 
 
+enum TypeSpeed { TURTLE, SLOW, NORMAL, FAST, RUSHED, INSTANT }
+
+const TYPE_SPEED = {
+	TypeSpeed.TURTLE: .3,
+	TypeSpeed.SLOW: .12,
+	TypeSpeed.NORMAL: .06,
+	TypeSpeed.FAST: .03,
+	TypeSpeed.RUSHED: .001,
+	TypeSpeed.INSTANT: .0,
+	}
+
 @export var icon: Texture2D
 @export var name: String
 @export var message: String
+@export var type_speed: TypeSpeed = TypeSpeed.NORMAL
+@export var punctuation_significance: float = 1.0
 @export var background: Color = Color(0.078, 0.106, 0.106)
 @export var angle: float
 @export var offset: Vector2
--- prefabs/dialogues/dialogue_system.gd
+++ prefabs/dialogues/dialogue_system.gd
@@ -5,6 +5,9 @@ extends CanvasLayer
 
 enum State { LOADING, HIDDEN, APPEARING, ACTIVE, HIDING }
 
+@export_group("Typing")
+@export var char_type_speed_multipliers: Dictionary[String, float]
+
 @export_group("Animations")
 @export var window_appear_duration: float = .3
 @export var window_hide_duration: float = .2
@@ -16,6 +19,8 @@ enum State { LOADING, HIDDEN, APPEARING, ACTIVE, HIDING }
 @onready var dialogue_name: Label = %Name
 @onready var dialogue_message: Label = %Message
 @onready var dialogue_background: ColorRect = %Background
+
+@onready var typing_timer: Timer = $Typing
 
 var state: State:
 	set(value):
@@ -33,11 +38,13 @@ var dialogue: Dialogue:
 		
 		dialogue_icon.texture = dialogue.icon
 		dialogue_name.text = dialogue.name
-		dialogue_message.text = dialogue.message
 		dialogue_background.color = dialogue.background
 		dialogue_window.rotation = deg_to_rad(dialogue.angle)
+		
 		_update_animation()
+		_start_typing_animation()
 var _active_tween: Tween # NOTE Using tweens for dynamic UI
+var _typing_index: int
 
 
 func _ready():
@@ -47,6 +54,24 @@ func _ready():
 func _input(event: InputEvent):
 	if event.is_action_pressed("dialogue_skip"):
 		close()
+
+
+func _on_character_typed():
+	type_next_character()
+
+
+func _get_character_type_speed(character: String) -> float:
+	var is_special_char := character in char_type_speed_multipliers
+	var char_type_speed_multiplier := (
+		char_type_speed_multipliers[character] if is_special_char else 1.0 )
+	
+	if not dialogue:
+		return char_type_speed_multiplier
+	
+	if is_special_char:
+		char_type_speed_multiplier *= dialogue.punctuation_significance
+	
+	return dialogue.TYPE_SPEED[dialogue.type_speed] * char_type_speed_multiplier
 
 
 func _update_animation():
@@ -60,6 +85,7 @@ func _update_animation():
 			dialogue_window.visible = false
 			dialogue_canvas.position = Vector2(0, 200.0)
 			dialogue_window.scale = Vector2(0.75, 0.75)
+			dialogue = null
 		
 		State.APPEARING:
 			dialogue_window.visible = true
@@ -99,14 +125,49 @@ func _update_animation():
 			)
 
 
+func _start_typing_animation():
+	if not dialogue:
+		return
+	match dialogue.type_speed:
+		dialogue.TypeSpeed.INSTANT:
+			show_whole_message()
+			return
+	
+	_typing_index = 0
+	dialogue_message.text = ""
+	type_next_character()
+
+
+func show_whole_message():
+	if not dialogue:
+		return
+	dialogue_message.text = dialogue.message
+
+
+func type_next_character():
+	if not dialogue or _typing_index >= len(dialogue.message):
+		return
+	
+	var new_char := dialogue.message[_typing_index]
+	var new_speed := _get_character_type_speed(new_char)
+	
+	if new_speed <= 0:
+		show_whole_message()
+		return
+	
+	dialogue_message.text += new_char
+	_typing_index += 1
+	typing_timer.start(new_speed)
+
+
 func display(new_dialogue: Dialogue):
 	if not new_dialogue:
 		return
 	
-	dialogue = new_dialogue
-	
 	match state:
 		State.HIDDEN: state = State.APPEARING
+	
+	dialogue = new_dialogue
 
 
 func close():
