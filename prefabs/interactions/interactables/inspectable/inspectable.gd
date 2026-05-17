a41e2e447353f38588695d632cd5b6e15b1ffed8
kesha <110396687+I-am-cat04@users.noreply.github.com>
2026-05-17 23:06:56 +0400

    feat: Add dialogue presets

--- prefabs/interactions/interactables/inspectable/inspectable.gd
+++ prefabs/interactions/interactables/inspectable/inspectable.gd
@@ -4,11 +4,20 @@ extends Interactable
 
 
 @export var dialogues: Array[Dialogue]
+@export var preset: Dialogue ## Default [member Dialogue.fallback] for [member dialogues].
 
 
 func _on_interaction() -> bool:
 	if not len(dialogues):
 		return false
 	
+	_resolve_preset()
+	
 	Game.dialogue_system.display(dialogues.pick_random())
 	return true
+
+
+func _resolve_preset():
+	for dialogue in dialogues:
+		if not dialogue.fallback:
+			dialogue.fallback = preset
