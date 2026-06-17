6ecdce58325e2e07c650dd42f29d3d83d9c20f86
Nickolay <karikh.nickolay@icloud.com>
2026-06-17 11:39:46 +0400

    feat: new arena for bamboo

--- prefabs/characters/bosses/demon_cyclop/ai_controller.gd
+++ prefabs/characters/bosses/demon_cyclop/ai_controller.gd
@@ -6,16 +6,21 @@ extends AIController
 
 
 func _ai_ready():
-	shoot_controller.post_shot_cd_started.connect(func(): movement_controller.enabled = false)
-	shoot_controller.post_shot_cd_finished.connect(func(): movement_controller.enabled = true)
+	pass
+	#shoot_controller.post_shot_cd_started.connect(func(): movement_controller.enabled = false)
+	#shoot_controller.post_shot_cd_finished.connect(func(): movement_controller.enabled = true)
 
 
 func _on_action(_action: AIAction):
+	if Game.player.reflection.damage_scale < 0:
+		Game.player.reflection.damage_scale *= -1
 	if _action.action_name == "bomb_and_regen" or _action.action_name == "fire_and_regen":
 		modifier.value = -1
 		modifier.duration = _action.duration
 		modifier.operation = modifier.Operation.multiply
 		Game.player.character.stat_damage_ratio.add_modifier(str(modifier.get_instance_id()), modifier)
+		if Game.player.reflection.damage_scale > 0:
+			Game.player.reflection.damage_scale *= -1
 		hurt_component.sounds_damage = creepy_sound
 		vfx.spawn(global_position)
 	else:
